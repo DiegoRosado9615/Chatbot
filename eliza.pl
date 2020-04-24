f17b85e70ab77e8217a2ec48525e79d7c1b17ab5
Izumi Sa <31079153+izu-s@users.noreply.github.com>
2020-04-23 22:58:39 -0500

    Update eliza.pl

--- eliza.pl
+++ eliza.pl
@@ -10,17 +10,21 @@ eliza(Stimuli, Response) :-
 
 template([s([i,am]),s(X)], [s([why,are,you]),s(X),w('?')]).
 template([w(i),s(X),w(you)], [s([why,do,you]),s(X),w(me),w('?')]).
+
 %%Suguerencias de diego
 %template([s([why,am,i]),s(X)], [s([you,are,cool,for,me])]).
-template([s([yo,quiero,comprar,un]),s(X)], [s([ seguro, quieres,un]),s(X),w('?')]).
-template([s([yo,quiero,viajar,a]),s(X)], [s([ seguro, quieres,ir, a ]),s(X),w('?')]).
-template([s([yo,quiero,comer,una]),s(X)], [s([ que, tan, sano ,es ,comer,una ]),s(X),w('?')]).
-template([s([me,siento]),s(X)], [s([ '¿',por, qué, te, sientes  ]),s(X),w('?')]).
-template([s([me, gusta , bailar ]),s(X)], [s([encerio,a , mi , me, ecanta, bailar   ]),s(X),w('?')]).
-template([s([te, gustan,los  ]),s(X)], [s([no, lo, siento, no,me,gustan  ]),s(X)]).
-template([s([has,visto  ]),s(X)], [s([si,me, encanta, ver  ]),s(X)]).
-template([s([conoces, a  ]),s(X)], [s([no, conosco,a  ]),s(X)]).
-template([s([te,has,enfermado,de  ]),s(X)], [s([soy,una,maquina, y ,no,puedo ,enfermarmem, de ]),s(X)]).
+template([s([mi,nombre,es]),s(X)], [s([hola]),s(X)],w(mi,nombre,es,eliza)).
+template([s([yo,quiero,comprar,un]),s(X)], [s(['¿',seguro,quieres,un]),s(X),w('?')]).
+template([s([yo,quiero,viajar,a]),s(X)], [s(['¿',seguro,quieres,ir,a]),s(X),w('?')]).
+template([s([yo,quiero,comer,una]),s(X)], [s(['¿',qué,tan,sano,es,comer,una]),s(X),w('?')]).
+template([s([me,siento]),s(X)], [s(['¿',por,qué,te,sientes]),s(X),w('?')]).
+template([s([me,gusta,bailar]),s(X)], [s(['¿'en serio,'?',a ,mí,me,encanta,bailar]),s(X)]).
+template([s([te,gustan,los]),s(X)], [s(lo,siento,no,me,gustan]),s(X)]).
+template([s([has,visto]),s(X)], [s([sí,me,encanta,ver]),s(X)]).
+template([s([sabes,cocinar]),s(X)], [s([soy,una,máquina,no,puedo,cocinar]),s(X)]).
+template([s([te,has,enfermado,de]),s(X)], [s([soy,una,máquina,y,no,puedo,enfermarme,de]),s(X)]).
+template([s([mi,serie,favorita,es]),s(X)], [s(['¡',mi,serie,favorita,también,es]),s(X)],w('!')).
+
 
 %%Mis suguerencias
 
